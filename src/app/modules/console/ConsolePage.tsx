dacbe5d7e1f540460726bd3c9287db7406f6ed18
TEMIRLAN <97175881+Temirlan1212@users.noreply.github.com>
2023-12-28 03:30:47 +0600

    add: init project

---
+++ src/app/modules/console/ConsolePage.tsx
@@ -0,0 +1,3 @@
+export function ConsolePage() {
+  return <>console</>;
+}
---
+++ src/app/modules/courses/CoursesPage.tsx
@@ -0,0 +1,44 @@
+import { PageTitle } from "../../../_metronic/layout/components/toolbar/page-title/PageTitle";
+import { PageLink } from "../../../_metronic/layout/core";
+import { Routes, Route, Outlet } from "react-router-dom";
+import { Courses } from "./components/Courses";
+
+const profileBreadCrumbs: Array<PageLink> = [
+  {
+    title: "Profile",
+    path: "/crafted/pages/profile/overview",
+    isSeparator: false,
+    isActive: false,
+  },
+  {
+    title: "",
+    path: "",
+    isSeparator: true,
+    isActive: false,
+  },
+];
+
+const CoursesPage = () => (
+  <Routes>
+    <Route
+      element={
+        <>
+          <></>
+          <Outlet />
+        </>
+      }
+    >
+      <Route
+        path="list"
+        element={
+          <>
+            <PageTitle breadcrumbs={profileBreadCrumbs}>List</PageTitle>
+            <Courses />
+          </>
+        }
+      />
+    </Route>
+  </Routes>
+);
+
+export default CoursesPage;
